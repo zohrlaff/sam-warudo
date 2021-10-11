94fd819f241799c2c07fdb044472661f3620774b
Sam Reyes <zohrlaffz@gmail.com>
2021-10-11 14:16:10 -0500

    Working Contact Page (!) Complete rework of Bootstrap with Bootstrap-Flask for Bootstrap 4 usability Flask pages and URLs complete

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Length, DataRequired, Email
 import datetime
+import smtplib
 
 
 class ContactForm(FlaskForm):
-    name = StringField(label='name', validators=[DataRequired()])
-    email = StringField(label='email', validators=[DataRequired(), Email()])
-    message = StringField(label='message', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    name = StringField(label='Name', validators=[DataRequired(), Length(min=2)])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    message = TextAreaField(label='Message', render_kw={"rows": 8}, validators=[DataRequired()])
+    submit = SubmitField('Send ↲')
 
 
 year = datetime.datetime.today().year
 app = Flask(__name__)
-# TODO 1. Move key to sys/os before deploying
+bootstrap = Bootstrap(app)
+# Form button customization
+app.config['BOOTSTRAP_BTN_STYLE'] = 'dark'
+app.config['BOOTSTRAP_BTN_SIZE'] = 'block'
+
+# TODO 1. Move this to sys/os before deploying
 app.secret_key = "pG^9KsXeg!LS9l*7s%Y8v90@N7wbIKoh"
+EMAIL = "joseph.table.k@outlook.com"
+MY_EMAIL = "zohrlaffz@gmail.com"
+PASSWORD = "c|10WKtCx"
+#############
+
+
+def send_email(name, email, message):
+    with smtplib.SMTP("smtp.live.com:587") as connection:
+        connection.ehlo()
+        connection.starttls()
+        connection.login(user=EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Contact Message!\n\nFrom: {name}\nEmail: {email}\n \n{message}"
+        )
+    return True
 
 
 @app.route('/')
@@ -30,11 +54,15 @@ def projects():
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    alert = None
     form = ContactForm()
-    # TODO 2. Fix contact form submission
     if form.validate_on_submit():
-        print(form.name.data, form.email.data, form.message.data)
-    return render_template("contact.html", year=year, form=form)
+        if send_email(name=form.name.data, email=form.email.data, message=form.message.data):
+            flash("Your message was sent successfully!")
+            return redirect(url_for('contact'))
+    else:
+        alert = "Please fill out the entire form!"
+    return render_template("contact.html", year=year, form=form, alert=alert)
 
 
 if __name__ == "__main__":
